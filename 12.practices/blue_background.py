c2492bf8c93216a30d03d4893f40298f7a91100c
qingqian <3036754502@qq.com>
2018-12-03 17:19:25 +0800

    修改代码

--- 12.practices/blue_background.py
+++ 12.practices/blue_background.py
@@ -6,7 +6,7 @@ def blue_background():
     screen = pygame.display.set_mode((1200, 600))
     pygame.display.set_caption("Blue Background")
     #纯蓝不大好看呀
-    bg_color = (25, 25, 255)
+    bg_color = (0, 0, 255)
     while True:
         screen.fill(bg_color)
         pygame.display.flip()
